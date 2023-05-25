789e9a7b0de858eeccaa57c371f9f26515d08c4a
Alexander Webber <alexwebber@ufl.edu>
2023-05-25 17:12:25 -0400

    Updated notebook, adding CSV and helpful scripts

---
+++ scripts/move_male_female.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+import shutil
+
+# Read the CSV file
+df = pd.read_csv("../data/mixtec_figures.csv")
+
+# Set the file locations
+fileOrigin = "../data/unlabeled_codices/codex_nuttall/cutouts/"
+fileDest = "../data/labeled_figures/codex_nutall/seated/"
+
+# Iterate through the file names
+for ff in df['file_name'].tolist():
+    file_codex = df['codex'].loc[df['file_name'] == ff]
+
+    # Check if the value in the "codex" column is equal to "codex_nutall"
+    if str(file_codex.values[0]) == "codex_nuttall":
+
+        # Get the gender from the CSV file
+        folder = df['seated'].loc[df['file_name'] == ff].values[0]
+
+        # Move the file to the destination folder
+        shutil.copy(fileOrigin + ff, fileDest + folder + "/")
+
+print("Files have been copied successfully!")
