522693b3b4296f056a177207b7ff1bb7f7375805
Alexander R. Webber <alexwebber@ufl.edu>
2023-08-16 00:58:52 -0400

    feat: updated files for aaai checkpoint; running parametric analysis over learning rate, batch size, and transforms.

--- runmodeltemplate.sh
+++ runmodeltemplate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=mixteclabeling
-#SBATCH --output=mixteclabeling.out
-#SBATCH --error=mixteclabeling--$learning_rate--$batch_size.err
+#SBATCH --output=mixteclabeling-%x.%j.out
+#SBATCH --error=mixteclabeling-%x.%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=alexwebber@ufl.edu
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=128gb
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64gb
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:a100:4
+#SBATCH --gres=gpu:a100:1
+#SBATCH --time=5:00:00
 
 #module load ufrc
 module load pytorch/2.0.1
@@ -20,4 +20,4 @@ date_run=$(date +"%F")
 echo "Learning rate: $learning_rate"
 echo "Batch size: $batch_size"
 ## add date as argument
-python3 src/train.py --learning_rate $learning_rate --batch_size $batch_size --run "$date_run--lr$learning_rate--bs$batch_size--transforms"
+python3 src/train.py --learning_rate $learning_rate --batch_size $batch_size --run "$date_run--lr$learning_rate--bs$batch_size--transformsNone"
